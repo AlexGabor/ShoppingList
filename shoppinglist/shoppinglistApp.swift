7213697df3ec26c8323673dad8ebd7adf68689c3
Alexandru Gabor <alexandru.gabor@halcyonmobile.com>
2022-01-07 12:10:52 +0200

    Initial Commit

---
+++ shoppinglist/shoppinglistApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppinglistApp.swift
+//  shoppinglist
+//
+//  Created by Alexandru Gabor on 07.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct shoppinglistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
